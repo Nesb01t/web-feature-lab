9d09e7a7ef80834cfe881746a8cc42cf150e12c7
nesb01t <nesb01t@163.com>
2024-02-15 02:02:22 +0800

    restore nx

---
+++ libs/vue-promise/src/promise.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { type PromiseStatus } from '../type';
+
+defineOptions({
+  name: 'Promise'
+});
+
+const props = defineProps<{}>();
+
+const status = ref<PromiseStatus>();
+</script>
+
+<template>
+  <template v-if="status == PromiseStatus.PENDING">
+    <slot name="loading" />
+  </template>
+  <template v-else-if="status == PromiseStatus.FULFILLED">
+    <slot name="success" />
+  </template>
+  <template v-else-if="status == PromiseStatus.REJECTED">
+    <slot name="error" />
+  </template>
+</template>
+
+<style scoped>
+
+</style>
---
+++ libs/vue-promise/type/index.ts
@@ -0,0 +1,5 @@
+export enum PromiseStatus {
+  PENDING,
+  FULFILLED,
+  REJECTED,
+}
