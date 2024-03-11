f90f18fefdb33e0c3becc7b4bd4f6616d5a510cc
nesb01t <nesb01t@163.com>
2024-03-11 21:24:54 +0800

    init vue-use-router

---
+++ apps/vue-use-router/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>hello there</div>
+</template>
---
+++ apps/vue-use-router/vite.config.ts
@@ -0,0 +1,34 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/vue-use-router',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  plugins: [vue(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  build: {
+    outDir: '../../dist/apps/vue-use-router',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
